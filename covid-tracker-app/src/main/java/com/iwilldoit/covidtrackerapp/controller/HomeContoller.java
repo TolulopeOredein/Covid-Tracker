4d9853dc5ccc9107967649a1b595487ea7e344aa
Jcoder <38234469+TolulopeOredein@users.noreply.github.com>
2021-10-14 23:03:37 +0100

    Add files via upload

---
+++ covid-tracker-app/src/main/java/com/iwilldoit/covidtrackerapp/controller/HomeContoller.java
@@ -0,0 +1,31 @@
+package com.iwilldoit.covidtrackerapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.iwilldoit.covidtrackerapp.CovidDataService;
+import com.iwilldoit.covidtrackerapp.model.LocationStats;
+
+@Controller
+public class HomeContoller {
+	
+	@Autowired
+	CovidDataService covidDataService;
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		List<LocationStats>allStats = covidDataService.getAllStats();
+		int totalReportedCases =allStats.stream().mapToInt(stat->stat.getLatestTotalCases()).sum();
+		int totalNewCases =allStats.stream().mapToInt(stat->stat.getDiffFromPrevDay()).sum();
+		model.addAttribute("locationStats", allStats);
+		model.addAttribute("totalReportedCases",totalReportedCases);
+		model.addAttribute("totalNewCases",totalNewCases);
+		
+		return "home";
+	}
+
+}
